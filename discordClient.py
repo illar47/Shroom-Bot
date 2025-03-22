aff0b7de577e08ef2b34e965acbbc04250b5a335
illar47 <irochez@comcast.net>
2025-03-22 17:34:17 -0400

    updating with more commands and stuff

--- discordClient.py
+++ discordClient.py
@@ -12,6 +12,8 @@ intents.message_content = True
 clientErrorStr = "<:errorIcon:1290854428991033465> **Error Occured** <:errorIcon:1290854428991033465>"
 
 client = commands.Bot(command_prefix='./', intents=intents)
+
+#Helper Command - formats an embed to be nice. 
 def embedFormatter(p_holderItem): 
     #TODO: formats an embed given a holdificator item and returns the embed - returns something else if failed
     #can differentiate beteen holder types
@@ -86,11 +88,23 @@ async def on_ready():
     print("Logged in as a bot {0.user}".format(client)) #TODO make actual log message
     await client.tree.sync()
 
+########### Commands - use discord embeds for these ###########
+
+#Test Command - Ping
 @client.hybrid_command(name="ping", help= "Test command for ensuring the bot is functioning as intended. Will respond with pong")
 async def ping(ctx):
     await ctx.send('Pong!')
 
-#Get Specific Item
+#Test Command - testing Optional Params
+@client.command(name="testOptionalParams", help="debugging method for optional parameters")
+async def testOptionalParams(ctx, *args): 
+    print("testing")
+    print(args)
+    print("done")
+    #await ctx.send(f'You passed {arg1} and {arg2}')
+    #need list of dictionary values that could autofill
+
+#Item Command - Get Specific Item
 @client.hybrid_command(name="grabitem", help="Returns information about specified item.")
 async def grabitem(ctx, p_item_name=None): 
     #check bag of Hoarding for Item
@@ -106,8 +120,7 @@ async def grabitem(ctx, p_item_name=None):
     else: 
          await ctx.send("<:errorIcon:1290854428991033465> **Error Occured** <:errorIcon:1290854428991033465> \n Sorry the item you have requested doesn't exist. Please try again")
 
-#Commands - use discord embeds for these
-#Get Random Item (with params)
+#Item Command - Get Random Item (with params)
 @client.hybrid_command(name="grabrandomitem", help="Grabs a random item that meets the parameters expectations")
 async def grabRandomItem(ctx, level=None, rarity=None, character=None): 
     #TODO: check if provided level, rarity, and characters are valid
@@ -129,8 +142,15 @@ async def grabRandomItem(ctx, level=None, rarity=None, character=None):
     else:
         await ctx.send("<:errorIcon:1290854428991033465> **Error Occured** <:errorIcon:1290854428991033465> \n Invalid Inputs provided. Please try again with different inputs")
 
-#Get Random NPC
-#Get Random Location / Quest
+#Item Command - sends a requested item to a specified player
+@client.hybrid_command(name="reqitem", help="sends requested item to specified player")
+async def reqItem(ctx, item_name=None, username=None):
+    #check that provided item exists - if no, then error
+    #find associated chat for user. send sheet there if exists. 
+    #error should be in bot channel, found sheet in character channel. 
+    print("TODO")
+
+#Location Command - Get Random Location / Quest
 @client.hybrid_command(name="grabrandomencounter", help="Grabs a random encounter that meets the parameters expectations")
 async def grabRandomEncounter(ctx, type=None): 
     if util.checkEncounterParamValidity(type):
@@ -148,7 +168,7 @@ async def grabRandomEncounter(ctx, type=None):
     else:
         await ctx.send("<:errorIcon:1290854428991033465> **Error Occured** <:errorIcon:1290854428991033465> \n Invalid Inputs provided. Please try again with different inputs")
 
-#Generate Random NPC
+#NPC Command - Generate Random NPC
 @client.hybrid_command(name="createnpc", help="Generates a Random NPC")
 async def createNPC(ctx, species=None, gender=None, description=None):
     npc:h.NPC = hcc.controlCenter.generateNPC(species, gender, description)        
@@ -161,42 +181,3 @@ async def createNPC(ctx, species=None, gender=None, description=None):
     else: 
         await ctx.send("<:errorIcon:1290854428991033465> **Error Occured** <:errorIcon:1290854428991033465> \n Sorry an error occured. Please try again momentarily")
 #TODO: Find way to specify what channel to send messages in.
-
-#Accept input
-
-
-############ IDEATION ###########################
-
-#determine if user is attempting to find a random location, item, or NPC
-
-    #if looking for item:
-
-        #determine identifying information: i.e. level, type (weapon, equipable etc), player tags (who it would suit)
-
-        #output random item that meets the criteria
-
-    #if looking for location
-
-        #determine if specific biome necessary (cave, beach, etc.)
-
-        #output random location that meets the criteria. 
-
-    #if looking for NPC
-        #determine identifying information: gender, age, career, etc. 
-
-        #randomly select name from list
-
-        #randomly select remaining information that was not specified by user (gender, age, career etc)
-
-        #maybe bug AI to quickly generate a 2 - 3 sentence character description? 
-        
-        #output results. 
-
-#### TEST LAND ####
-@client.command(name="testOptionalParams", help="debugging method for optional parameters")
-async def testOptionalParams(ctx, *args): 
-    print("testing")
-    print(args)
-    print("done")
-    #await ctx.send(f'You passed {arg1} and {arg2}')
-    #need list of dictionary values that could autofill
